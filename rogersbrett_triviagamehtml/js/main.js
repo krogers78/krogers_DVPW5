e7356f687279c2cce4fe6a3b8cef97b1e19a82f0
Brett Rogers <bdrogers@fullsail.edu>
2017-11-05 21:00:34 -0500

    Ability to answer a question correctly has been implemented

--- rogersbrett_triviagamehtml/js/main.js
+++ rogersbrett_triviagamehtml/js/main.js
@@ -3,24 +3,6 @@
 
 window.addEventListener('load', e => {
   this.game = Game.getInstance()
-
-  function captureResults(data) {
-    let htmlString = ''
-    if (data[0].type == 'boolean') {
-      htmlString = ` <h3>${data[0].question}</h3>
-                        <label><input type="radio" name="answer" id="answer1" value="${data[0].answer_array[0]}">${data[0].answer_array[0]}</label>
-                        <label><input type="radio" name="answer" id="answer2" value="${data[0].answer_array[1]}">${data[0].answer_array[1]}</label>
-                        <button id="finalAnswer">Submit</button>`                            
-    } else {
-      htmlString = `<h3>${data[0].question}</h3>
-                        <label><input type="radio" name="answer" id="answer1" value="${data[0].answer_array[0]}">${data[0].answer_array[0]}</label>
-                        <label><input type="radio" name="answer" id="answer2" value="${data[0].answer_array[1]}">${data[0].answer_array[1]}</label>
-                        <label><input type="radio" name="answer" id="answer3" value="${data[0].answer_array[2]}">${data[0].answer_array[2]}</label>
-                        <label><input type="radio" name="answer" id="answer4" value="${data[0].answer_array[3]}">${data[0].answer_array[3]}</label>
-                        <button id="finalAnswer">Submit</button>`
-    }
-    document.querySelector('body').innerHTML = htmlString
-   }
 })
 
 class Game {
--- rogersbrett_triviagamehtml/js/mvc.js
+++ rogersbrett_triviagamehtml/js/mvc.js
@@ -1,44 +1,67 @@
+
 class Controller {
   constructor() {
     this.model = new Model()
     this.view = new View()
     // variables for later use
-    this.oName = ''
-    this.tName = ''
-    this.category = ''
-    this.difficulty = ''
+    this.oName = document.querySelector('#pOneName')
+    this.tName = document.querySelector('#pTwoName')
+    this.category = document.querySelector('#categoryPicker')
+    this.difficulty = document.querySelector('#difficultyPicker')
     this.playerData = new PlayerData()
+    this.counter = 0
 
     document.querySelector('#submitBtn').addEventListener('click', this.captureForm.bind(this))
+    document.addEventListener('apiGet', this.startIt.bind(this))
+    document.addEventListener('done', this.getChoice.bind(this))
   }
+  // capture the data from the users input
   captureForm(e) {
     e.preventDefault()
-    this.playerData.oName = document.querySelector('#pOneName').value
-    this.playerData.tName = document.querySelector('#pTwoName').value
-    this.playerData.category = document.querySelector('#categoryPicker').value
-    this.playerData.difficulty = document.querySelector('#difficultyPicker').value
-
+    this.playerData.oName = this.oName.value
+    this.playerData.tName = this.tName.value
+    this.playerData.category = this.category.value
+    this.playerData.difficulty = this.difficulty.value
+    
     let evt = new Event('dataGet')
     evt.data = this.playerData
     document.dispatchEvent(evt)
   }
-  getData() {
-    let newData = []
-    let url = 'https://opentdb.com/api.php?amount=10'
-    let options = { method: "GET" }
-
-    fetch(url, options)
-      .then(response => response.json())
-      .then(responseAsJSON => {
-        newData = responseAsJSON.results
-        newData.forEach(e => {
-          e.answer_array = Utils.shuffle([...e.incorrect_answers, e.correct_answer])
-        })
-      })
-      .catch(error => {
-        console.log('An Error Occured: ', error)
-      })
-   }
+  startIt(e) {
+    console.log(e)
+    let evt = new Event('startGame')
+    evt.query = e.questions[0]
+    document.dispatchEvent(evt)
+  }
+  getChoice(e) {
+    e.preventDefault()
+    let correct = e.thatQuestion.correct_answer
+    let type = e.thatQuestion.type
+    console.log('TYPE',type)
+    document.querySelector('#confirmBtn').addEventListener('click', e => {
+      if (type == 'boolean') {
+        if (document.querySelector('#answer1').checked && document.querySelector('#answer1').value == correct) {
+          console.log('Correct Answer!')
+        } else if (document.querySelector('#answer2').checked && document.querySelector('#answer2').value == correct) {
+          console.log('Correct Answer')
+        } else {
+          console.log('Incorrect Answer!')
+        }
+      } else {
+        if (document.querySelector('#answer1').checked && document.querySelector('#answer1').value == correct) {
+          console.log('Correct Answer!')
+        } else if (document.querySelector('#answer2').checked && document.querySelector('#answer2').value == correct) {
+          console.log('Correct Answer')
+        } else if (document.querySelector('#answer3').checked && document.querySelector('#answer3').value == correct) {
+          console.log('Correct Answer!')
+        } else if (document.querySelector('#answer4').checked && document.querySelector('#answer4').value == correct) {
+          console.log('Correct Answer')
+        } else {
+          console.log('Incorrect Answer!')
+        }
+      }
+    })
+  }
 }
 class Model {
   constructor() {
@@ -58,18 +81,71 @@ class Model {
       url += `&difficulty=${setupInfo.difficulty}`
     }
     console.log(url)
+    let newData = []
 
     fetch(url, options)
       .then(response => response.json())
       .then(responseAsJSON => {
-        console.log('FETCH', responseAsJSON.results)
+        newData = responseAsJSON.results
+        newData.forEach(e => {
+          e.answer_array = Utils.shuffle([...e.incorrect_answers, e.correct_answer])
+        })
+        console.log(newData)
+
+        let evt = new Event('apiGet')
+        evt.questions = newData
+        document.dispatchEvent(evt)
       })
-      .catch(err => {
-        throw `An Error Occured: ${err}`
+      .catch(error => {
+        console.log('An Error Occured: ', error)
       })
+      
   }
 }
 class View {
   constructor() {
+    document.addEventListener('startGame', this.startGame.bind(this))
+  }
+  startGame(e) {
+    let question = e.query
+    if (question.type == 'boolean') {
+      let htmlString = `<h1>${question.question}</h1>
+                      <label>
+                        ${question.answer_array[0]}
+                        <input type="radio" name="answerSelect" id="answer1" value="${question.answer_array[0]}">
+                      </label>
+                      <label>
+                        ${question.answer_array[1]}
+                        <input type="radio" name="answerSelect" id="answer2" value="${question.answer_array[1]}">
+                      </label>
+                      <input type="button" id="confirmBtn" value="Confirm">`
+      document.querySelector('body').innerHTML = htmlString
+      
+    } else {
+      let htmlString = `<h1>${question.question}</h1>
+                      <label>
+                        ${question.answer_array[0]}
+                        <input type="radio" name="answerSelect" id="answer1" value="${question.answer_array[0]}">
+                      </label>
+                      <label>
+                        ${question.answer_array[1]}
+                        <input type="radio" name="answerSelect" id="answer2" value="${question.answer_array[1]}">
+                      </label>
+                      <label>
+                        ${question.answer_array[2]}
+                        <input type="radio" name="answerSelect" id="answer3" value="${question.answer_array[2]}">
+                      </label>
+                      <label>
+                        ${question.answer_array[3]}
+                        <input type="radio" name="answerSelect" id="answer4" value="${question.answer_array[3]}">
+                      </label>
+                      <input type="button" id="confirmBtn" value="Confirm">`
+      document.querySelector('body').innerHTML = htmlString
+    }
+
+
+    let evt = new Event('done')
+    evt.thatQuestion = e.query
+    document.dispatchEvent(evt)
   }
 }
