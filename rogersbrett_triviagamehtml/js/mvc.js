03ade1f725b62bbc5a8d26febd20e0ce96ade2af
Brett Rogers <bdrogers@fullsail.edu>
2017-11-07 09:28:48 -0500

    Fixed an issue with incorrect answers

--- rogersbrett_triviagamehtml/js/mvc.js
+++ rogersbrett_triviagamehtml/js/mvc.js
@@ -1,4 +1,3 @@
-
 class Controller {
   constructor() {
     this.model = new Model()
@@ -9,11 +8,12 @@ class Controller {
     this.category = document.querySelector('#categoryPicker')
     this.difficulty = document.querySelector('#difficultyPicker')
     this.playerData = new PlayerData()
-    this.counter = 0
+    this.index = 0
+    this.pTurn = 0
 
     document.querySelector('#submitBtn').addEventListener('click', this.captureForm.bind(this))
     document.addEventListener('apiGet', this.startIt.bind(this))
-    document.addEventListener('done', this.getChoice.bind(this))
+    document.addEventListener('first', this.getChoice.bind(this))
   }
   // capture the data from the users input
   captureForm(e) {
@@ -25,41 +25,75 @@ class Controller {
     
     let evt = new Event('dataGet')
     evt.data = this.playerData
+    evt.index = this.index
+    evt.turn = this.pTurn
     document.dispatchEvent(evt)
   }
   startIt(e) {
-    console.log(e)
-    let evt = new Event('startGame')
-    evt.query = e.questions[0]
+    let evt = new Event('qShow')
+    evt.questions = e.questions
+    evt.index = e.index
+    evt.players = e.players
+    evt.turn = e.turn
     document.dispatchEvent(evt)
   }
   getChoice(e) {
     e.preventDefault()
-    let correct = e.thatQuestion.correct_answer
-    let type = e.thatQuestion.type
-    console.log('TYPE',type)
+    let data = e
+    let correct = e.questions[e.index].correct_answer
+    
     document.querySelector('#confirmBtn').addEventListener('click', e => {
-      if (type == 'boolean') {
+      let cOrI = true;
+      if (data.questions[data.index].type == 'boolean') {
         if (document.querySelector('#answer1').checked && document.querySelector('#answer1').value == correct) {
-          console.log('Correct Answer!')
+          cOrI = true
         } else if (document.querySelector('#answer2').checked && document.querySelector('#answer2').value == correct) {
-          console.log('Correct Answer')
+          cOrI = true
         } else {
-          console.log('Incorrect Answer!')
+          cOrI = false
         }
       } else {
         if (document.querySelector('#answer1').checked && document.querySelector('#answer1').value == correct) {
-          console.log('Correct Answer!')
+          cOrI = true
         } else if (document.querySelector('#answer2').checked && document.querySelector('#answer2').value == correct) {
-          console.log('Correct Answer')
+          cOrI = true
         } else if (document.querySelector('#answer3').checked && document.querySelector('#answer3').value == correct) {
-          console.log('Correct Answer!')
+          cOrI = true
         } else if (document.querySelector('#answer4').checked && document.querySelector('#answer4').value == correct) {
-          console.log('Correct Answer')
+          cOrI = true
         } else {
-          console.log('Incorrect Answer!')
+          cOrI = false
         }
       }
+      if (cOrI) {
+        // determines who's turn it is to answer
+        if (data.turn == 0) {
+          data.players.playerOne.score++
+          console.log('Player One Score', data.players.playerOne.score)
+          data.turn = 1
+        } else {
+          data.players.playerTwo.score++
+          data.turn = 0
+        }
+
+        this.index++
+        console.log('Correct Answer!')
+      } else {
+        console.log('Incorrect Answer!')
+        if (data.turn == 0) {
+          data.turn = 1
+        } else {
+          data.turn = 0
+        }
+      }
+      // Event that fires once the question has been answered
+      let evt = new Event('qShow')
+      evt.index = this.index
+      evt.questions = data.questions
+      // evt.names = names
+      evt.players = data.players
+      evt.turn = data.turn
+      document.dispatchEvent(evt)
     })
   }
 }
@@ -82,7 +116,7 @@ class Model {
     }
     console.log(url)
     let newData = []
-
+    // Fetch the questions based off user input
     fetch(url, options)
       .then(response => response.json())
       .then(responseAsJSON => {
@@ -90,10 +124,23 @@ class Model {
         newData.forEach(e => {
           e.answer_array = Utils.shuffle([...e.incorrect_answers, e.correct_answer])
         })
-        console.log(newData)
-
+        // create the variable to store the player information
+        let playerInfo = {
+          playerOne: {
+            name: setupInfo.oName,
+            score: 0
+          },
+          playerTwo: {
+            name: setupInfo.tName,
+            score: 0
+          }
+        }
+        // Event that fires once all the information is gathered
         let evt = new Event('apiGet')
         evt.questions = newData
+        evt.index = e.index
+        evt.players = playerInfo
+        evt.turn = e.turn
         document.dispatchEvent(evt)
       })
       .catch(error => {
@@ -104,48 +151,62 @@ class Model {
 }
 class View {
   constructor() {
-    document.addEventListener('startGame', this.startGame.bind(this))
+    document.addEventListener('qShow', this.showQuestion.bind(this))
+    // document.addEventListener('correctAns', this.nextQuestion.bind(this))
   }
-  startGame(e) {
-    let question = e.query
-    if (question.type == 'boolean') {
-      let htmlString = `<h1>${question.question}</h1>
+  showQuestion(e) {
+    let question = e.questions
+    let i = e.index
+    let players = e.players
+    if (question[i].type == 'boolean') {
+      let htmlString = `<h1>${question[i].question}</h1>
                       <label>
-                        ${question.answer_array[0]}
-                        <input type="radio" name="answerSelect" id="answer1" value="${question.answer_array[0]}">
+                        ${question[i].answer_array[0]}
+                        <input type="radio" name="answerSelect" id="answer1" value="${question[i].answer_array[0]}">
                       </label>
                       <label>
-                        ${question.answer_array[1]}
-                        <input type="radio" name="answerSelect" id="answer2" value="${question.answer_array[1]}">
+                        ${question[i].answer_array[1]}
+                        <input type="radio" name="answerSelect" id="answer2" value="${question[i].answer_array[1]}">
                       </label>
                       <input type="button" id="confirmBtn" value="Confirm">`
-      document.querySelector('body').innerHTML = htmlString
+      document.querySelector('#main').innerHTML = htmlString
       
     } else {
-      let htmlString = `<h1>${question.question}</h1>
+      let htmlString = `<h1>${question[i].question}</h1>
                       <label>
-                        ${question.answer_array[0]}
-                        <input type="radio" name="answerSelect" id="answer1" value="${question.answer_array[0]}">
+                        ${question[i].answer_array[0]}
+                        <input type="radio" name="answerSelect" id="answer1" value="${question[i].answer_array[0]}">
                       </label>
                       <label>
-                        ${question.answer_array[1]}
-                        <input type="radio" name="answerSelect" id="answer2" value="${question.answer_array[1]}">
+                        ${question[i].answer_array[1]}
+                        <input type="radio" name="answerSelect" id="answer2" value="${question[i].answer_array[1]}">
                       </label>
                       <label>
-                        ${question.answer_array[2]}
-                        <input type="radio" name="answerSelect" id="answer3" value="${question.answer_array[2]}">
+                        ${question[i].answer_array[2]}
+                        <input type="radio" name="answerSelect" id="answer3" value="${question[i].answer_array[2]}">
                       </label>
                       <label>
-                        ${question.answer_array[3]}
-                        <input type="radio" name="answerSelect" id="answer4" value="${question.answer_array[3]}">
+                        ${question[i].answer_array[3]}
+                        <input type="radio" name="answerSelect" id="answer4" value="${question[i].answer_array[3]}">
                       </label>
                       <input type="button" id="confirmBtn" value="Confirm">`
-      document.querySelector('body').innerHTML = htmlString
+      document.querySelector('#main').innerHTML = htmlString
+    }
+    document.querySelector('#scores').innerHTML = `<h3>${players.playerOne.name}</h3>
+                                                  <p>Score: ${players.playerOne.score}</p>
+                                                  <h3>${players.playerTwo.name}</h3>
+                                                  <p>Score: ${players.playerTwo.score}</p>`
+    if (e.turn == 0) {
+      console.log(`${players.playerOne.name}'s Turn!`)
+    } else {
+      console.log(`${players.playerTwo.name}'s Turn!`)      
     }
 
-
-    let evt = new Event('done')
-    evt.thatQuestion = e.query
+    let evt = new Event('first')
+    evt.questions = e.questions
+    evt.index = i
+    evt.players = players
+    evt.turn = e.turn
     document.dispatchEvent(evt)
   }
 }
